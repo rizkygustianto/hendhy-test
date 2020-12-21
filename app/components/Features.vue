f71351c27dad86e35bebe088a4fd2d80d9e0910e
rizkygustianto <rizkyrebellion@gmail.com>
2020-12-21 17:50:11 +0700

    Project bootstrapping

---
+++ app/components/Features.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="bg-white border-b py-8">
+    <div class="container max-w-5xl mx-auto m-8">
+      <h1
+        class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800"
+      >
+        What We Do
+      </h1>
+      <div class="w-full mb-4">
+        <div
+          class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"
+        ></div>
+      </div>
+      <feature-right
+        headline="Validate and Sharpen Your Ideas"
+        content="We take your ideas and make sure it's valid, marketable, and foolproof."
+      >
+        <img src="~assets/img/travel-booking.svg?inline" alt slot="svg" />
+      </feature-right>
+      <feature-left
+        headline="Connecting You To Users and Strategic Partners"
+        content="We have over 100+ partners for you to connect with, enabling you to supercharge your business ideas."
+      >
+        <img src="~assets/img/connected-world.svg?inline" alt slot="svg" />
+      </feature-left>
+    </div>
+  </section>
+</template>
+
+<script>
+import FeatureRight from '@/components/FeatureRight'
+import FeatureLeft from '@/components/FeatureLeft'
+
+export default {
+  name: 'Features',
+  components: {
+    'feature-right': FeatureRight,
+    'feature-left': FeatureLeft
+  }
+}
+</script>
---
+++ app/pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header />
+    <features />
+    <teasers />
+    <prices />
+    <call-to-action />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Features from '@/components/Features'
+import Teasers from '@/components/Teasers'
+import Prices from '@/components/Prices'
+import CallToAction from '@/components/CallToAction'
+
+export default {
+  name: 'About',
+  components: {
+    header: Header,
+    features: Features,
+    teasers: Teasers,
+    prices: Prices,
+    'call-to-action': CallToAction
+  }
+}
+</script>
